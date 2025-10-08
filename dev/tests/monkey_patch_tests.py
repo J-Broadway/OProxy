d0d6ca7c3e8b17a569ec2517c2b07ab7c0e5ca62
Jeremiah <jeremiah.broadway@hotmail.com>
2025-10-07 22:06:22 -0500

    test update

--- dev/tests/monkey_patch_tests.py
+++ dev/tests/monkey_patch_tests.py
@@ -6,3 +6,13 @@ log = tf.log
 tf.info('Begin testing monkey patch functionality')
 tf.info('Clearing storage')
 opr._clear()
+
+tf.info("Adding containe opr._add('items', tf.mvs) ")
+opr._add('items', tf.mvs)
+tf.info("Showing storage after _add()")
+opr._storage()
+tf.info('Now going to monkey patch the "items" container')
+opr._extend('items', cls='ResolutionMP', dat='extensions_for_tests', monkey_patch=True)
+tf.info('Now going to call the monkey patched container')
+opr.items('op1').resolution()
+
